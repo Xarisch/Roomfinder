09cc2c9c14211f4c61315dc08a3787b4825120aa
Räto Ulc <raeto.ulc@stud.th-deg.de>
2026-05-18 12:38:01 +0200

    introduced frontend; removed test __init__ in router.py

--- backend/main.py
+++ backend/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI, HTTPException
+from fastapi.middleware.cors import CORSMiddleware
 
 #own imports
 from app.modules.router import router as Find_Router
@@ -6,6 +7,18 @@ from app.modules.router import router as Find_Router
 
 app = FastAPI()
 
+
+origins= [
+    "http://localhost:3000"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(Find_Router)
 
