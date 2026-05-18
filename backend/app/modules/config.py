2ac46d5704d82f36c853771c938b208303a757a2
Räto Ulc <raeto.ulc@stud.th-deg.de>
2026-05-18 11:09:14 +0200

    Bugfixing; Find room not working yet

--- backend/app/modules/config.py
+++ backend/app/modules/config.py
@@ -1,5 +1,12 @@
-DATA_FILE = "./backend/app/json/data.json"
-COMPLETE_DATA = "./backend/app/json/complete_data.json"
+import os
 
+#JSON paths
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+JSON_DIR = os.path.normpath(os.path.join(BASE_DIR, "../json"))
+
+DATA_FILE     = os.path.join(JSON_DIR, "data.json")
+COMPLETE_DATA = os.path.join(JSON_DIR, "complete_data.json")
+
+#Thabella
 THABELLA_BACKEND_MAIN_URL = "https://thabella.th-deg.de/thabella/opn/period/findByDate/"
 THABELLA_PERIOD_LIST = "https://thabella.th-deg.de/thabella/opn/period/list"
--- backend/main.py
+++ backend/main.py
@@ -1,10 +1,13 @@
 from fastapi import FastAPI, HTTPException
 
 #own imports
-
+from app.modules.router import router as Find_Router
 
 
 app = FastAPI()
+
+
+app.include_router(Find_Router)
 
 @app.get("/hello")
 def hello():
